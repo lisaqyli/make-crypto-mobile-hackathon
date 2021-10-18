4cf0bf4af667fa1fd054580a4989309289805d8c
Charlie Andrews <charlie.andrews@valoraapp.com>
2021-10-17 21:21:28 -0700

    fixes for farm-bot.sol

--- farm-bot/codebase/contracts/farm-bot.sol
+++ farm-bot/codebase/contracts/farm-bot.sol
@@ -2,18 +2,19 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
 contract FarmBot {
     address public owner;
 
     constructor(address _owner) {
-        console.log("Deploying a Greeter with owner:", _owner);
+        console.log("Deploying a FarmBot with owner:", _owner);
         owner = _owner;
     }
 
     // deposits can be done without a dedicated method (just send funds to the contract address)
 
-    function withdraw(address token, uint256 amount) {
+    function withdraw(address token, uint256 amount) public {
         require(msg.sender == owner, "Only the owner can withdraw");
         IERC20(token).transfer(msg.sender, amount);
     }
