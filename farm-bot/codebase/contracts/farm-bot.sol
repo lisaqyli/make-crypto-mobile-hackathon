d15a57b9b00fe5e2900b4fdf75ebae21a3e6149d
Charlie Andrews <charlie.andrews@valoraapp.com>
2021-11-05 20:22:55 -0700

    refactored to use 'farm points' for user balances

--- farm-bot/codebase/contracts/farm-bot.sol
+++ farm-bot/codebase/contracts/farm-bot.sol
@@ -5,24 +5,44 @@ import "hardhat/console.sol";
 import "./ubeswap-farming/contracts/StakingRewards.sol"; // does the ierc20 import too
 
 contract FarmBot {
-    mapping(address => uint256) private _balances;
+    mapping(address => uint256) private fpBalances; // maps address to "Farm Point" balances.
+    // FP balances ONLY changes on deposits/withdrawals, NOT when FarmBot claims rewards. This helps save gas.
+    // NOTE: the thing that DOES change is the VALUE of an FP relative to an LP token
+
+    uint256 lpTotalBalance; // total number of LP tokens owned by Farm Bot
+    uint256 fpTotalBalance; // total number of "Farm Points" issued by Farm Bot
 
     address public tokenAddress = 0xf3098223845F37Ffe4B3A066F2D38A0587317269; // mcUSD-Celo
     StakingRewards public farm = StakingRewards(0x299f31f48D4667a6f68E4331dB05212d57Cc7f80); // alfajores farm for mcUSD-Celo
 
-    function deposit(uint256 amount) public {
-        // todo might need a lock on this
-        bool transferSuccess = IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
-        require(transferSuccess, "Transfer failed, aborting deposit");
-        _balances[msg.sender] += amount;
+    function getFpAmount(uint256 _lpAmount) public view returns (uint256) {
+        // todo check the rounding on this at some point...
+        if (lpTotalBalance == 0) {
+            return 1;
+        }
+        return _lpAmount * fpTotalBalance / lpTotalBalance;
     }
 
-    function withdraw(uint256 amount) public {
+    function deposit(uint256 _lpAmount) public {
         // todo might need a lock on this
-        require(_balances[msg.sender] >= amount, "Only the owner can withdraw");
-        bool transferSuccess = IERC20(tokenAddress).transfer(msg.sender, amount);
+        bool transferSuccess = IERC20(tokenAddress).transferFrom(msg.sender, address(this), _lpAmount);
+        require(transferSuccess, "Transfer failed, aborting deposit");
+        uint256 _fpAmount = this.getFpAmount(_lpAmount);
+        fpTotalBalance += _fpAmount;
+        lpTotalBalance += _lpAmount;
+        fpBalances[msg.sender] += _fpAmount;
+    }
+
+    function withdraw(uint256 _lpAmount) public {
+        // todo might need a lock on this
+        uint256 _fpAmount = this.getFpAmount(_lpAmount);
+        require(fpBalances[msg.sender] >= _fpAmount, "Cannot withdraw more than the total balance of the owner");
+        // todo withdraw LP from farm to make sure farm bot has the balance to do the transfer
+        bool transferSuccess = IERC20(tokenAddress).transfer(msg.sender, _lpAmount);
         require(transferSuccess, "Transfer failed, aborting withdrawal");
-        _balances[msg.sender] -= amount;
+        fpBalances[msg.sender] -= _fpAmount;
+        fpTotalBalance -= _fpAmount;
+        lpTotalBalance -= _lpAmount;
     }
 
     function investInFarm() public {
