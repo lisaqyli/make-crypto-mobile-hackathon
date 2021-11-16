934e87864f8787c8726529db3ccc902a31b461d9
Charlie Andrews <charlie.andrews@valoraapp.com>
2021-11-15 21:43:08 -0800

    WIP deposits seem to work at least

--- farm-bot/codebase/contracts/farm-bot.sol
+++ farm-bot/codebase/contracts/farm-bot.sol
@@ -5,12 +5,12 @@ import "hardhat/console.sol";
 import "./ubeswap-farming/contracts/StakingRewards.sol"; // does the ierc20 import too
 
 contract FarmBot {
-    mapping(address => uint256) private fpBalances; // maps address to "Farm Point" balances.
+    mapping(address => uint256) public fpBalances; // maps address to "Farm Point" balances. todo eventually make private
     // FP balances ONLY changes on deposits/withdrawals, NOT when FarmBot claims rewards. This helps save gas.
     // NOTE: the thing that DOES change is the VALUE of an FP relative to an LP token
 
-    uint256 lpTotalBalance; // total number of LP tokens owned by Farm Bot
-    uint256 fpTotalBalance; // total number of "Farm Points" issued by Farm Bot
+    uint256 public lpTotalBalance; // total number of LP tokens owned by Farm Bot
+    uint256 public fpTotalBalance; // total number of "Farm Points" issued by Farm Bot
 
     address public tokenAddress = 0xf3098223845F37Ffe4B3A066F2D38A0587317269; // mcUSD-Celo
     StakingRewards public farm = StakingRewards(0x299f31f48D4667a6f68E4331dB05212d57Cc7f80); // alfajores farm for mcUSD-Celo
@@ -19,23 +19,26 @@ contract FarmBot {
         // todo check the rounding on this at some point...
         if (lpTotalBalance == 0) {
             return _lpAmount;
+        } else {
+            return _lpAmount * fpTotalBalance / lpTotalBalance;
         }
-        return _lpAmount * fpTotalBalance / lpTotalBalance;
     }
 
     function deposit(uint256 _lpAmount) public {
         // todo might need a lock on this
         bool transferSuccess = IERC20(tokenAddress).transferFrom(msg.sender, address(this), _lpAmount);
         require(transferSuccess, "Transfer failed, aborting deposit");
+
         uint256 _fpAmount = this.getFpAmount(_lpAmount);
         fpTotalBalance += _fpAmount;
         lpTotalBalance += _lpAmount;
         fpBalances[msg.sender] += _fpAmount;
-        investInFarm();
+        //        investInFarm(); // todo uncomment once debugging complete
     }
 
     function withdraw(uint256 _lpAmount) public {
         // todo might need a lock on this
+        require(_lpAmount > 0, "Must withdraw positive amount");
         uint256 _fpAmount = this.getFpAmount(_lpAmount);
         require(fpBalances[msg.sender] >= _fpAmount, "Cannot withdraw more than the total balance of the owner");
 
--- farm-bot/codebase/scripts/check-farm-bot-alfajores.js
+++ farm-bot/codebase/scripts/check-farm-bot-alfajores.js
@@ -1,9 +1,11 @@
-const {newKit, StableToken} = require('@celo/contractkit')
+const {newKit} = require('@celo/contractkit')
 const assert = require('assert')
 const FARM_BOT_ABI = require('../abis/farmBot.json')
+const {WrapperCache} = require("@celo/contractkit/lib/contract-cache");
 
 const FORNO_ALFAJORES_URL = 'https://alfajores-forno.celo-testnet.org'
-const FARM_BOT_ADDRESS_ALFAJORES = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+const FARM_BOT_ADDRESS_ALFAJORES = '0x78bA5A050a5A8b257a91cd8299D34D0643520Df5'
+const TOKEN_ADDRESS = '0xf3098223845F37Ffe4B3A066F2D38A0587317269' // mcUSD-Celo LP
 
 async function getKit(privateKey) {
   const kit = await newKit(FORNO_ALFAJORES_URL)
@@ -16,8 +18,8 @@ async function getKit(privateKey) {
 
 async function approve(kit, amount) {
   const walletAddress = kit.web3.eth.defaultAccount
-  const cusdContract = await kit.contracts.getStableToken(StableToken.cUSD)
-  const approveTx = await cusdContract.approve(FARM_BOT_ADDRESS_ALFAJORES, amount).send({
+  const tokenContract = await (new WrapperCache(kit)).getErc20(TOKEN_ADDRESS)
+  const approveTx = await tokenContract.approve(FARM_BOT_ADDRESS_ALFAJORES, amount).send({
     from: walletAddress,
     gas: 50000,
     gasPrice: 1000000000
@@ -30,10 +32,11 @@ function getFarmBotContract(kit) {
 }
 
 async function deposit(kit, amount) {
+  // NOTE: this invests in a farm now!
   const farmBotContract = getFarmBotContract(kit)
   return farmBotContract.methods.deposit(amount).send({
     from:kit.web3.eth.defaultAccount,
-    gas: 99000,
+    gas: 1076506,
     gasPrice: 1000000000,
   })
 }
@@ -41,6 +44,15 @@ async function deposit(kit, amount) {
 async function withdraw(kit, amount) {
   const farmBotContract = getFarmBotContract(kit)
   return farmBotContract.methods.withdraw(amount).send({
+    from: kit.web3.eth.defaultAccount,
+    gas: 99000,
+    gasPrice: 1000000000,
+  })
+}
+
+async function invest(kit) {
+  const farmBotContract = getFarmBotContract(kit)
+  return farmBotContract.methods.investInFarm().send({
     from: kit.web3.eth.defaultAccount,
     gas: 99000,
     gasPrice: 1000000000,
@@ -61,23 +73,26 @@ async function withdraw(kit, amount) {
  */
 async function main() {
   const kit1 = await getKit(process.env.ALFAJORES_WALLET_PRIVATE_KEY)
-  const amount = kit1.web3.utils.toWei('0.1', 'ether')
+  const amount = kit1.web3.utils.toWei('0.01', 'ether')
+
+  console.log(`approving for account ${kit1.web3.eth.defaultAccount}`)
   const approveResult = await approve(kit1, amount)
   assert.equal(approveResult.status, true, 'Unexpected approve result')
 
+  console.log(`depositing LP for account ${kit1.web3.eth.defaultAccount}`)
   const depositResult = await deposit(kit1, amount)
   assert.equal(depositResult.status, true, 'Unexpected deposit result')
 
-  const kit2 = await getKit(process.env.ALFAJORES_WALLET_PRIVATE_KEY_2)
-  try {
-    await withdraw(kit2, amount) // should fail (none deposited!)
-  } catch (error) {
-    if (!error.message.includes('Transaction has been reverted by the EVM')) {
-      throw error
-    }
-  }
-  const user1withdrawResult = await withdraw(kit1, amount) // should pass
-  assert.equal(user1withdrawResult.status, true, 'User 1 should be able to withdraw since they deposited already')
+  // const kit2 = await getKit(process.env.ALFAJORES_WALLET_PRIVATE_KEY_2)
+  // try {
+  //   await withdraw(kit2, amount) // should fail (none deposited!)
+  // } catch (error) {
+  //   if (!error.message.includes('Transaction has been reverted by the EVM')) {
+  //     throw error
+  //   }
+  // }
+  // const user1withdrawResult = await withdraw(kit1, amount) // should pass
+  // assert.equal(user1withdrawResult.status, true, 'User 1 should be able to withdraw since they deposited already')
 }
 
-main()
+main().catch(console.error)
